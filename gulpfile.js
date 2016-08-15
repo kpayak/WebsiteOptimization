c90f69bad6190edbc9848023e07574ee3845733a
kpayak <payak.keyur@gmail.com>
2016-08-14 21:27:52 -0700

    updated after page speed score of 95

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+
  
 gulp.task('cssminify', function () {
     gulp.src(['./css/*.css','./views/css/*.css'])
@@ -20,9 +24,23 @@ gulp.task('htmlminify', function() {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src(['./js/*.js','./views/js/*.js']),
+        uglify(),
+        gulp.dest('./dist/js')
+    ],
+    cb
+  );
+});
+
 gulp.task('watch', function() {
     gulp.watch('./*.html', ['htmlminify']);
     gulp.watch('./views/*.html', ['htmlminify']);
     gulp.watch('./css/*.css', ['cssminify']);
     gulp.watch('./views/css/*.css', ['cssminify']);
+    gulp.watch('./js/*.js',['compress']);
+    gulp.watch('./views/js/*.js',['compress']);
 });
+
+gulp.task('default', ['htmlminify', 'cssminify', 'compress']);
