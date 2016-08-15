8d8f31342c80aeb83fd45f742db1a45f216cfbf3
kpayak <payak.keyur@gmail.com>
2016-08-14 20:55:03 -0700

    mid check in

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,25 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
  
 gulp.task('cssminify', function () {
-    gulp.src('./css/*.css')
-    gulp.src('./views/css/*.css')
+    gulp.src(['./css/*.css','./views/css/*.css'])
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('htmlminify', function() {
-  gulp.src('*.html')
-  gulp.src('./views/*.html')
-  //gulp.src('./views/*.html')
+  gulp.src(['*.html','./views/*.html'])
     .pipe(htmlmin({
         collapseWhitespace: true,
         minifyCSS:true,
         minifyJS:true
         }))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./*.html', ['htmlminify']);
+    gulp.watch('./views/*.html', ['htmlminify']);
+    gulp.watch('./css/*.css', ['cssminify']);
+    gulp.watch('./views/css/*.css', ['cssminify']);
 });
